de8e8885b7c0f9a40c1a61e1be4f386e60fa8b77
Tóth Gergely <t.gergely2001@gmail.com>
2021-12-02 18:39:27 +0100

    main menu started

--- M4YFLU_HFT_2021221.Client/Program.cs
+++ M4YFLU_HFT_2021221.Client/Program.cs
@@ -11,36 +11,161 @@ namespace M4YFLU_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            System.Threading.Thread.Sleep(8000);
+            System.Threading.Thread.Sleep(5000);
+
+
+            //RestService rest = new RestService("http://localhost:26250");
+            //rest.Post<Brand>(new Brand()
+            //{
+            //    Name = "Peugeot"
+            //}, "brand");
+
+
+            //var brands = rest.Get<Brand>("brand");
+            //var cars = rest.Get<Car>("car");
+
+            //var ownerwiththemostexpensivecar = rest.GetSingle<IEnumerable<KeyValuePair<string, string>>>("stat/ownerwiththemostexpensivecar");
+            //var luigiscars = rest.GetSingle<IEnumerable<KeyValuePair<string, string>>>("stat/luigiscars");
+            //var vwowners = rest.GetSingle<IEnumerable<Owner>>("stat/vwowners");
+            //var ownersfrombp = rest.GetSingle<IEnumerable<KeyValuePair<Owner, string>>>("stat/ownersfrombp");
+            //var expensivecars = rest.GetSingle<IEnumerable<KeyValuePair<string, int>>>("stat/expensivecars");
+
+
+
+            //Console.WriteLine(brands);
+            //Console.WriteLine("\n\n" + cars);
 
             RestService rest = new RestService("http://localhost:26250");
-            rest.Post<Brand>(new Brand()
+
+
+
+            Menu(rest);
+
+            var q = rest.Get<Brand>("/brand");
+            ;
+
+        }
+
+
+        static void Menu(RestService rest)
+        {
+
+
+            Console.WriteLine("Choose from the tables below: ");
+            Console.WriteLine("1:  Brand table");
+            Console.WriteLine("2:  Car table");
+            Console.WriteLine("3:  Owner table");
+            Console.WriteLine("4: Close the program");
+            int x = int.Parse(Console.ReadLine());
+            if (x == 1)
             {
-                Name = "Peugeot"
-            }, "brand");
+
+                Brandtable(rest);
+                //var q = rest.Get<Brand>("brand");
 
 
-            var brands = rest.Get<Brand>("brand");
-            var cars = rest.Get<Car>("car");
+            }
+            else if (x == 2)
+            {
+                CarTable();
+                //var q = rest.Get<Brand>("car");
+            }
+            else if (x == 3)
+            {
+                OwnerTable();
+                //var q = rest.Get<Brand>("owner");
+            }
+            else if (x == 4)
+            {
 
-            var ownerwiththemostexpensivecar = rest.GetSingle<IEnumerable<KeyValuePair<string, string>>>("stat/ownerwiththemostexpensivecar");
-            var luigiscars = rest.GetSingle<IEnumerable<KeyValuePair<string, string>>>("stat/luigiscars");
-            var vwowners = rest.GetSingle<IEnumerable<Owner>>("stat/vwowners");
-            var ownersfrombp = rest.GetSingle<IEnumerable<KeyValuePair<Owner, string>>>("stat/ownersfrombp");
-            var expensivecars = rest.GetSingle<IEnumerable<KeyValuePair<string, int>>>("stat/expensivecars");
+            }
+            else
+            {
+                Console.WriteLine("invalid option! Try again: ");
+                Menu(rest);
+            }
 
-            ;
-
-            Console.WriteLine(brands);
-            Console.WriteLine("\n\n" + cars);
-
-            ;
-
-            //IEnumerable<Owner> res = rest.GetSingle<IEnumerable<Owner>>("stat/ownerwiththemostexpensivecar");
-
-            ;
 
 
         }
+
+        static void Brandtable(RestService rest)
+        {
+            Console.WriteLine("Select a method from the Brand table: ");
+            Console.WriteLine("1: Create ");
+            Console.WriteLine("2: Delete");
+            Console.WriteLine("3: Read");
+            Console.WriteLine("4: Update");
+            Console.WriteLine("5: GetAll");
+            Console.WriteLine("6: Back to main menu");
+            int x = int.Parse(Console.ReadLine());
+
+
+
+
+            if (x == 1)
+            {
+                Console.WriteLine("Add a new brand name: ");
+                string name = Console.ReadLine();
+                Brand b1 = new Brand() { Name = name };
+                rest.Post<Brand>(b1, "/brand");
+                Brandtable(rest);
+            }
+            else if (x == 2)
+            {
+                Console.WriteLine("Enter an Id: ");
+                string q = Console.ReadLine();
+                int w = int.Parse(q);
+                rest.Delete(w, $"/brand{q}");
+                Brandtable(rest);
+            }
+            else if (x == 3)
+            {
+                Console.WriteLine("Enter an Id: ");
+                string q = Console.ReadLine();
+                var c = rest.GetSingle<Brand>($"/brand/{q}");
+                Console.WriteLine(c);
+                Brandtable(rest);
+
+            }
+            else if (x == 4)
+            {
+                Console.WriteLine("Enter an Id: ");
+                string q = Console.ReadLine();
+                rest.Put<Brand>(rest.GetSingle<Brand>($"/brand/{q}"), "/brand");
+                Brandtable(rest);
+            }
+            else if (x == 5)
+            {
+                rest.Get<Brand>("/brand");
+                Brandtable(rest);
+            }
+            else if (x == 6)
+            {
+                Menu(rest);
+            }
+            else
+            {
+                Console.WriteLine("invalid option!");
+                Menu(rest);
+            }
+
+
+
+        }
+
+        static void CarTable()
+        {
+
+        }
+
+        static void OwnerTable()
+        {
+
+        }
+
+
+
+
     }
 }
